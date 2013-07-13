a132654449d7ddc57f6cc9e37e45778ec60fcf4b
Eric Prunier <eric.prunier@gmail.com>
2013-07-13 17:49:21 +0200

    Exclude doc from master branch

--- project.clj
+++ project.clj
@@ -6,5 +6,4 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [clj-http "0.6.5"]
                  [cheshire "5.0.2"]]
-  :codox {:output-dir "doc/api"
-          :exclude clak.core})
+  :codox {:exclude clak.core})
